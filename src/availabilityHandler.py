4866ece47d893291e05e63914fa3e86484d8a43a
Andrew Simonson <asimonson1125@gmail.com>
2021-09-22 14:37:07 -0400

    Created mass availability check and score

---
+++ src/availabilityHandler.py
@@ -0,0 +1,36 @@
+import recurring_ical_events
+import datetime
+
+
+def availableFor(calendars, start, end):
+    """
+    takes an array of calendars and the start/end times of a hypothetical meeting
+    Returns an array of tuples (calendar, availability boolean)
+    """
+    if start == end: # if start and end are the same, 'between' will return true regardless of if an event is ongoing.  We don't want that. 
+        end += datetime.timedelta(seconds=1)
+    availability = []
+    for i in calendars:
+        if len(recurring_ical_events.of(i).between(start, end)) == 0: # no conflicting events were found
+            availability.append((i,True))
+        else:
+            availability.append((i,False))
+    return availability
+
+
+def availabilityScore(availabilities, scores):
+    """
+    Could probably be called unavailability score, since it is rating the number
+    of people who CAN'T make it.  
+
+    availabilities should be an array of tuples a la availableFor()
+    scores[i] should be an int representing the value of the availability
+    of the calendar at availabilities[i][0]
+    """
+    score = 0
+    for i in range(len(availabilities)): # for each calendar
+        if not availabilities[i][1]: # if unavailable
+            if scores[i] == -1: # -1 represents a mandatory attendance
+                return -1 # a required participant can't make this time
+            score += scores[i]
+    return score
--- tests/printEvents.py
+++ tests/printEvents.py
@@ -1,6 +1,8 @@
 # And the creator said: Here I will make a test, so that all may see that I am competent
 # and Lo, the test was made and impressed nobody, though viability was proven.
 
+# NO SRC DEPENDANCIES REQUIRED
+
 import urllib.request
 import recurring_ical_events
 import datetime
@@ -12,10 +14,11 @@ ical_string = urllib.request.urlopen(url).read()
 calendar = icalendar.Calendar.from_ical(ical_string)
 # --------------------------------------------------------------------------------------------
 
-events = recurring_ical_events.of(calendar).between(datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(hours=12)) # Gets calendar timeline between now and now+2days as an array
+events = recurring_ical_events.of(calendar).between(datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(hours=12)) 
+# Gets calendar timeline between now and now+2days as an array
 
 for i in range(len(events)): # For each event in the next 2 days...
     eName = events[i]['SUMMARY'] # Event name in string
-    eStart = events[i]['DTSTART'].dt # Event start time in Arrow
-    eEnd = events[i]['DTEND'].dt # Event duration in timedelta
+    eStart = events[i]['DTSTART'].dt # Event start time in dt
+    eEnd = events[i]['DTEND'].dt # Event end time in dt
     print(eName, "\n\tStarts: ", eStart, "\n\tDuration: ", eEnd - eStart, '\n')
