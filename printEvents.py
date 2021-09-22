795cda1003c235cb08ce255f112c75f1ed403342
Andrew Simonson <asimonson1125@gmail.com>
2021-09-22 13:26:51 -0400

    update example

--- printEvents.py
+++ printEvents.py
@@ -6,22 +6,13 @@ import recurring_ical_events
 import datetime
 import icalendar
 
-
-def deltaConversion(td): # output: HH:MM
-    minutes = (td.seconds % 3600 //60)
-    if (minutes < 10):
-        minutes = "0" + str(minutes)
-    else:
-        minutes = str(minutes)
-    return str(td.seconds // 3600) + ":" + minutes
-
 # Gets calendar from google in ICS file ------------------------------------------------------
 url = "https://calendar.google.com/calendar/ical/abs1907%40g.rit.edu/public/basic.ics"
 ical_string = urllib.request.urlopen(url).read()
 calendar = icalendar.Calendar.from_ical(ical_string)
 # --------------------------------------------------------------------------------------------
 
-events = recurring_ical_events.of(calendar).between(datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(hours=12)) # Gets calendar timeline between now and now+2days as a list
+events = recurring_ical_events.of(calendar).between(datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(hours=12)) # Gets calendar timeline between now and now+2days as an array
 
 for i in range(len(events)): # For each event in the next 2 days...
     eName = events[i]['SUMMARY'] # Event name in string
