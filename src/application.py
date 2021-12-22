f2a5bbb47a1bcf6061c21d3d7aa89d413a90dcd2
Andrew Simonson <asimonson1125@gmail.com>
2021-12-21 23:53:39 -0500

    single calendar status with full input
    still all text

--- src/application.py
+++ src/application.py
@@ -1,7 +1,7 @@
 import flask
 import datetime
 import calc
-
+from dateutil import parser
 
 app = flask.Flask(__name__)
 
@@ -18,13 +18,13 @@ def run():
    if flask.request.method == 'POST':
       urls = []
       in_scores = []
-      start = datetime.datetime.now() - datetime.timedelta(hours=9)
-      end = datetime.datetime.now() - datetime.timedelta(hours=2)
-      interval = datetime.timedelta(minutes=15)
-      length = datetime.timedelta(hours=2)
+      start = parser.parse(flask.request.form['startTime'])
+      end = parser.parse(flask.request.form['endTime'])
+      interval = datetime.timedelta(minutes=int(flask.request.form['interval']))
+      length = datetime.timedelta(minutes=int(flask.request.form['duration']))
       # for i in
       urls.append(flask.request.form['Calendar 1'])
-      in_scores.append(5)
+      in_scores.append(int(flask.request.form['Score 1']))
       output = calc.run(urls, in_scores, start, end, interval, length)
       return output
 
