f593e7ff60e3767557bb6318fcd99ab12ace68d6
Andrew Simonson <asimonson1125@gmail.com>
2022-06-23 21:08:22 -0400

    refactor test modules

--- tests/timedelta_sandbox.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import math
-
-time = datetime.timedelta(minutes=5)
-
-print(int((time).total_seconds() / 60))
-for i in range(int((time).total_seconds() / 60)):
-    print(datetime.timedelta(minutes=i))
